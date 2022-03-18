6016bd45be310dff9a09746d364d743c7a960108
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-18 13:42:14 -0400

    removed useless commented out code

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// // Arrays to keep track of each task's state
-// const taskTitles = [];
-// const taskComplete = [];
-// const taskDescriptions = [];
-
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
 function newTask(title, description) {
   const task = {
     title: title,
@@ -19,7 +12,6 @@ function newTask(title, description) {
   };
   return task;
 }
-// DRIVER CODE BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
